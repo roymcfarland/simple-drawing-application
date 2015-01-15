7a29d334122f7501332933b2c6b3a51bcab4e197
Roy McFarland <roy.mcfarland@gmail.com>
2015-01-14 21:25:11 -0700

    Added new color selector and appended result to DOM

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var $color = $('.selected').css("background-color");
 
-// When clicking on control list items
-$('.controls li').on('click', function() {
+// When clicking on list items
+$(document).on('click', '.controls li', function() {
 	// Deselect sibling elements
 	$(this).siblings().removeClass('selected');
 	// Select clicked elements
@@ -12,14 +12,35 @@ $('.controls li').on('click', function() {
 
 
 // When new color button is clicked
+$('#revealColorSelect').on('click', function() {
 	// Show or hide the color select
+	changeColor();
+	$('#colorSelect').toggle();
+});
+
+
+// Update the new color <span>
+var changeColor = function() {
+	var r = $('#red').val();
+	var g = $('#green').val();
+	var b = $('#blue').val();
+	$('#newColor').css('background-color', 'rgb(' + r + ',' + g + ',' + b + ')');
+};
+
 
 // When color sliders change
-	// Update the new color <span>
+$('input[type=range]').on('change', changeColor);
 
 // When add color is clicked
+$('#addNewColor').on('click', function() {
 	// Append color to the controls <ul>
+	var $newColor = $('<li></li>');
+	$newColor.css('background-color', $('#newColor').css('background-color'));
+	$('.controls ul').append($newColor);
 	// Select the new color automatically
+	$newColor.click();
+});
+
 
 // On mouse events on the <canvas>
 	// Draw lines
