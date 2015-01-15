ade825396880e467a69dda88141612a6fa0703f6
Roy McFarland <roy.mcfarland@gmail.com>
2015-01-14 20:53:47 -0700

    Added interactivity to color button selectors

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
-$(document).on('ready', function() {
-  
+var $color = $('.selected').css("background-color");
+
+// When clicking on control list items
+$('.controls li').on('click', function() {
+	// Deselect sibling elements
+	$(this).siblings().removeClass('selected');
+	// Select clicked elements
+	$(this).addClass('selected');
+	// Cache current color
+	color = $(this).css('background-color');
 });
+
+
+// When new color button is clicked
+	// Show or hide the color select
+
+// When color sliders change
+	// Update the new color <span>
+
+// When add color is clicked
+	// Append color to the controls <ul>
+	// Select the new color automatically
+
+// On mouse events on the <canvas>
+	// Draw lines
